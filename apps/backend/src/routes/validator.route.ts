54ea7dcd4599121c72f041c099718a548590fda9
Sarthak Italiya <sarthak.italiya2005@gmail.com>
2025-10-12 02:28:19 +0530

    feat: add auth middleware to validator routes

--- apps/backend/src/routes/validator.route.ts
+++ apps/backend/src/routes/validator.route.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { getValidatorById, getValidators } from "../controllers/validator.controller";
+import { authMiddleware } from "../middleware/auth.middleware";
+
 const router = express.Router();
 
+router.use(authMiddleware);
 
 router.get("/", getValidators);
-router.get("/:id", getValidatorById)
+router.get("/:id", getValidatorById);
 
 export default router;
